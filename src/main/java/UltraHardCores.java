a0fe4a85276febac2e82a3c0a31b390db36e5307
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-23 15:44:13 +0300

    Java 8 support

--- src/main/java/UltraHardCores.java
+++ src/main/java/UltraHardCores.java
@@ -1,5 +1,8 @@
+import arc.struct.Seq;
 import arc.util.Timer;
 import mindustry.Vars;
+import mindustry.game.Teams.*;
+import mindustry.gen.Building;
 import mindustry.mod.Plugin;
 
 public class UltraHardCores extends Plugin {
@@ -7,9 +10,9 @@ public class UltraHardCores extends Plugin {
     @Override
     public void init() {
         Timer.schedule(() -> {
-            var teams = Vars.state.teams.present;
-            for (var team : teams) {
-                for (var core : team.cores) {
+            Seq<TeamData> teams = Vars.state.teams.present;
+            for (TeamData team : teams) {
+                for (Building core : team.cores) {
                     core.health = Float.POSITIVE_INFINITY;
                 }
             }
